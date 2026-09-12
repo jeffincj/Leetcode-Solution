cd8caeed69e4cf86a5b3f41f923f8c466311ca83
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-09-12 09:52:21 +0530

    Time: 17 ms (25.97%) | Memory: 12.3 MB (56.03%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if x<0:
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
-        sam=x
-        rev=0
-        while sam:
-            d=sam%10
-            rev=rev*10+d
-            sam//=10
-        return rev==x
+
+        rev = 0
+        while x > rev:             
+            rev = rev * 10 + x % 10
+            x //= 10
+
+        return x == rev or x == rev // 10
