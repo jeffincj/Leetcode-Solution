d5397493af716418721a1c17cb11f75128ea194e
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-09-16 08:19:48 +0530

    Time: 1 ms (96.30%) | Memory: 47.4 MB (60.04%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total =0;
+        for (int n :nums){
+            total+=n;
+        }
+        int l=0;
+        for (int i=0;i<nums.length;i++){
+            int r=total-l-nums[i];
+            if (l==r)
+            return i;
+            l+=nums[i];
+        }
+        return -1;
+    }
+}
