e05674aa0bd0019487bf49e39dced29d882e2d64
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-09-16 22:28:42 +0530

    Time: 11 ms (50.86%) | Memory: 14.5 MB (11.58%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,13 @@
-from collections import Counter
 class Solution(object):
     def majorityElement(self, nums):
-        d=Counter(nums)
-        res=0
-        p=0
-        for i in d:
-            if res<d[i]:
-                p=i
-                res=d[i]
-               
-        return p
-        
+        can=nums[0]
+        c=1
+        for i in range(1,len(nums)):
+            if can==nums[i]:
+                c+=1
+            else:
+                c-=1
+            if c==0:
+                can=nums[i]
+                c=1
+        return can       
