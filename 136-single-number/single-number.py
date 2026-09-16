a97642c388be07fe9fe6ac901bd09add9789c893
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-09-16 06:04:37 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (53.26%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,8 @@
 class Solution(object):
     def singleNumber(self, nums):
-        res= defaultdict(int)
+        res=0
         for i in nums:
-            res[i]=res.get(i,0)+1
-        for i in res.keys():
-            if res[i]==1:
-                return i
+            res^=i
+        return res
+        
         
