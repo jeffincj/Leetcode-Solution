56bae2136f4156a9deba922695ecefa72e05908c
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-09-17 20:24:35 +0530

    Time: 5 ms (4.76%) | Memory: 43.6 MB (7.91%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String pr="";
+        boolean valid = true;
+        for(char c:strs[0].toCharArray()){
+            
+            pr+=c;
+            for (int j=1;j<strs.length;j++){
+                if (!strs[j].startsWith(pr)){
+                    return pr.substring(0, pr.length() - 1);
+                }
+            }
+        }
+        return pr;
+    }
+}
