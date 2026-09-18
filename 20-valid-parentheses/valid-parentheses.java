72b1c48d2899b39b3b627d0979722abc2ddcd5b3
Jeffin C John <cj.jeffincjohn@gmail.com>
2026-09-18 06:29:53 +0530

    Time: 3 ms (85.94%) | Memory: 43.7 MB (9.81%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character>p=new ArrayDeque<>();
+        for (char c:s.toCharArray()){
+            if (c=='('||c=='{'||c=='[') p.push(c);
+            else{
+                if(p.isEmpty())return false;
+                char top=p.pop();
+                if ((c==')'&& top!='(')||(c=='}'&& top!='{')||(c==']'&& top!='['))return false;
+                }
+            }
+            if (!p.isEmpty())return false;
+            return true;
+        }
+    }
